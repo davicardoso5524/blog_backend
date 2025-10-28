2688bfb1b702ed3af79bfc50d234681ea90ddab0
davicardoso5524 <davicardoso524@academico.ufs.br>
2025-10-28 12:32:16 -0300

    Liberando site

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const postController = require('../controllers/post.controller');
 const { authenticate, isAdmin, isPublisher } = require('../middlewares/auth.middleware');
 
+// ROTA PÚBLICA - SEM authenticate
+router.get('/', postController.listPosts);
+router.get('/slug/:slug', postController.getPostBySlug);
+
 // Todas as rotas precisam de autenticação
 router.use(authenticate);
 
 // Rotas para PUBLISHER e ADMIN
 router.post('/', isPublisher, postController.createPost);
-router.get('/', postController.listPosts);
-router.get('/slug/:slug', postController.getPostBySlug);
 router.get('/:id', postController.getPostById);
 router.put('/:id', postController.updatePost);
 router.delete('/:id', postController.deletePost);
